46d16a6079c6ea4d7ad75e9008e6ca29a76f90b8
johnmesh <john.mesh4@gmail.com>
2023-01-12 14:57:00 +0300

    show loader in the homepage

--- src/pages/launch/home.page.tsx
+++ src/pages/launch/home.page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
+import Spinner from '/public/Spinner.svg'
 import Home from '/public/images/home.svg'
+import { useDataContext } from 'hooks/useDataProvider'
+import moment from 'moment'
 
 export default function LastLaunchPage() {
+  const { loading, launchNext, launchLatest } = useDataContext()
+
+  const isLoading =
+    !launchLatest?.launch_date_local && !launchNext?.launch_date_local
   return (
     <div className="pb-16">
       <div className="bg-[#0f4a75] h-72 flex m-auto items-center">
@@ -13,19 +20,29 @@ export default function LastLaunchPage() {
       </div>
 
       <div className="flex flex-col justify-center items-center ">
-        <div className="mt-8 w-96 flex justify-between">
-          <span className="cursor-pointer text-[#42A5F5] uppercase">
-            <Link href="/launch/last-launch">Last Rocket Launch</Link>
-          </span>
-          <span>12/13/30392</span>
-        </div>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <div className="mt-8 w-96 flex justify-between">
+              <span className="cursor-pointer text-[#42A5F5] uppercase">
+                <Link href="/launch/last-launch">Last Rocket Launch</Link>
+              </span>
+              <span>
+                {moment(launchLatest?.launch_date_local).format('MM-DD-YYYY')}
+              </span>
+            </div>
 
-        <div className="mt-8 w-96 flex justify-between uppercase">
-          <span className="cursor-pointer text-[#42A5F5]">
-            <Link href="/launch/next-launch"> Next Rocket Launch</Link>
-          </span>
-          <span>12/13/30392</span>
-        </div>
+            <div className="mt-8 w-96 flex justify-between uppercase">
+              <span className="cursor-pointer text-[#42A5F5]">
+                <Link href="/launch/next-launch"> Next Rocket Launch</Link>
+              </span>
+              <span>
+                {moment(launchNext?.launch_date_local).format('MM-DD-YYYY')}
+              </span>
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
